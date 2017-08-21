09f35790fd50c6561bc7d4a68b282e3229a6a842
Perly Gustafsson <contact@daperley.com>
2017-08-21 11:04:06 +0200

    Seed of user

---
+++ PizzaButikenOnline/Data/DBInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PizzaButikenOnline.Models;
+
+namespace PizzaButikenOnline.Data
+{
+    public class DBInitializer
+    {
+        public static void Initialize(UserManager<ApplicationUser> userManager)
+        {
+            var aUser = new ApplicationUser();
+
+            aUser.UserName = "test@user.com";
+            aUser.Email = "test@user.com";
+
+            var r = userManager.CreateAsync(aUser, "Abc12#Test");
+        }
+    }
+}
--- PizzaButikenOnline/Startup.cs
+++ PizzaButikenOnline/Startup.cs
@@ -35,12 +35,13 @@ namespace PizzaButikenOnline
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
+            services.AddTransient<UserManager<ApplicationUser>>();
 
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, UserManager<ApplicationUser> userManager)
         {
             if (env.IsDevelopment())
             {
@@ -63,6 +64,8 @@ namespace PizzaButikenOnline
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            DBInitializer.Initialize(userManager);
         }
     }
 }
