c6081099ec4b902c16ab21011c64c6b71591df28
Perly Gustafsson <contact@daperley.com>
2017-08-30 09:29:18 +0200

    Models for cart added

---
+++ PizzaButikenOnline/Models/Cart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PizzaButikenOnline.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+        public virtual ICollection<CartItem> CartItems { get; set; }
+    }
+}
---
+++ PizzaButikenOnline/Models/DishViewModels/DishMenuViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PizzaButikenOnline.Models.DishViewModels
+{
+    public class DishMenuViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+
+        // For the actuall store set ingredients
+        public ICollection<IngredientDish> IngredientDish { get; set; }
+
+        // For ingredients added by the customer
+        public ICollection<Ingredient> Ingredents { get; set; }
+    }
+}
