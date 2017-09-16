0c76a65e2194ad8b6962bafe95245f6f77e153ae
Perly Gustafsson <contact@daperley.com>
2017-09-16 15:41:09 +0200

    DishRepository added

---
+++ PizzaButikenOnline/Repositories/DishRepository.cs
@@ -0,0 +1,76 @@
+﻿using PizzaButikenOnline.Data;
+using PizzaButikenOnline.Models;
+using System.Linq;
+
+namespace PizzaButikenOnline.Repositories
+{
+    public class DishRepository : IRepository<Dish>
+    {
+        private ApplicationDbContext _context;
+
+        public DishRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Create(Dish item)
+        {
+            try
+            {
+                _context.Dishes.Add(item);
+
+                SaveChanges();
+            }
+            catch (System.Exception)
+            {
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                _context.Dishes.Remove(Get(id));
+
+                SaveChanges();
+            }
+            catch (System.Exception)
+            {
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public Dish Get(int id)
+        {
+
+            return _context.Dishes.FirstOrDefault(d => d.Id == id);
+        }
+
+        public IQueryable<Dish> List()
+        {
+            return _context.Dishes.AsQueryable();
+        }
+
+        public bool SaveChanges()
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ PizzaButikenOnline/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace PizzaButikenOnline.Repositories
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> List();
+        bool Create(T item);
+        bool Delete(int id);
+        T Get(int id);
+        bool SaveChanges();
+    }
+}
